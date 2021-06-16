1ccdcbb6475f40cbacbb040cfe06cc4205c72829
chetan98 <rawatchetan133@gmail.com>
2021-06-16 06:11:30 +0530

    Skills tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const navMenu = document.getElementById('nav-menu'),
 /* Validate if constanst exists */
 if(navToggle){
     navToggle.addEventListener('click',() =>{
-        navMenu.classList.add('show-me')
+        navMenu.classList.add('show-menu')
     })
 }
 
@@ -15,11 +15,11 @@ if(navToggle){
 /* Validate if constanst exists */
 if(navClose){
     navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-me')
+        navMenu.classList.remove('show-menu')
     })
 }
 
-/* ============ Remove Menu Mobile ============*/
+/* ================= Remove Menu Mobile ============*/
 // removes menu once an option is choosen from menu
 const navLink = document.querySelectorAll('.nav__link')
 function linkAction(){
@@ -28,3 +28,46 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== TECH SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    
+    console.log(itemClass);
+    for(i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
